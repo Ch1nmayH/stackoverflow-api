a24888c6344a2f5d3770d4a412b75090521db55f
Chinmay Hegde <cod1.1backup@gmail.com>
2025-01-07 13:45:07 +0530

    Added new controller for stackoverflow Api for clean code.

--- routes/stackOverFlowRoute.js
+++ routes/stackOverFlowRoute.js
@@ -1,10 +1,10 @@
 import express from 'express'
+import stackOverFlowController from '../controllers/stackOverFlowController.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Stackoverflow API is up.')
-})
+router.post('/questions/load', stackOverFlowController.getStackOverFlowData);
+
 
 export default router
 
