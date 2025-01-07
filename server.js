e3fcc8a06ed0e099de592985859d89f42b7f0621
Chinmay Hegde <cod1.1backup@gmail.com>
2025-01-07 12:58:02 +0530

    Setup the basic boilerplate for the project and connected to the MongoDB database.

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Backend is up.')
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port,  () => {
+    connectDB();
+    console.log(`Server is running on port ${port}`)
+})
+
