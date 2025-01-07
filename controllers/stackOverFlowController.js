e16eb08e6d7d84e412a13e58c79c65a7d7e23b42
Chinmay Hegde <cod1.1backup@gmail.com>
2025-01-07 14:33:47 +0530

    Changed the functionality to not delete before loading new data and accept 100 records at a time with non duplicate records and added the filtering as well for getting the questions.

--- controllers/stackOverFlowController.js
+++ controllers/stackOverFlowController.js
@@ -3,12 +3,18 @@ import axios from "axios";
 
 const getStackOverFlowData = async (req, res) => {
   try {
+    const url = `https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow&pagesize=100&page=1`
     const response = await axios.get(
-      "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
+        url
     );
     const data = response.data.items;
-    const deleteData = await stackOverFlow.deleteMany();
+    // const deleteData = await stackOverFlow.deleteMany();
     data.map(async (item) => {
+     const alreadyExists = await stackOverFlow.findOne({question_id: item.question_id, title: item.title, link: item.link});
+        if(alreadyExists) {
+            // console.log("Already Exists");
+            return;
+        }
       const stackOverFlowData = await stackOverFlow.create({
         tags: item.tags,
         owner: item.owner,
@@ -26,8 +32,6 @@ const getStackOverFlowData = async (req, res) => {
       });
 
       const stackOverFlowQuestions = await stackOverFlow.find();
-
-      
     });
     res.status(201).json({ message: "Questions Loaded successfully" });
   } catch (error) {
@@ -35,32 +39,99 @@ const getStackOverFlowData = async (req, res) => {
   }
 };
 
-
 const getStackOverFlowQuestions = async (req, res) => {
-    try {
-        res.send("Questions");
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        
+  try {
+    const id = req.params.id;
+    let {
+      is_answered,
+      tags,
+      answers_count__gt,
+      answers_count__lt,
+      sort,
+      page,
+    } = req.query;
+
+    if (id) {
+      const stackOverFlowQuestion = await stackOverFlow.findById(id);
+      return res.send(stackOverFlowQuestion);
     }
-}
+
+    let filter = {};
+
+    if (is_answered) {
+      filter.is_answered = is_answered == "true" ? true : false;
+    }
+
+    if (tags) {
+        filter.tags = {$in: tags.split(",")}
+    }
+
+    if (answers_count__gt) {
+        filter.answer_count = {$gt: parseInt(answers_count__gt)}
+    }
+
+    if (answers_count__lt) {
+        filter.answer_count = {$lt: parseInt(answers_count__lt)}
+    }
+
+    let sortFilter = {};
+
+    if (sort) {
+        if(sort === "score".toLocaleLowerCase()) {
+            sortFilter.score = -1
+        }
+
+        if(sort === "created_at".toLocaleLowerCase()) {
+            sortFilter.creation_date = -1
+        }
+    }
+
+    const limit = 100;
+    if(!page) {
+        page = 1;
+    }
+    const skip =  (page - 1) * limit;
+
+    const totalQuestions = await stackOverFlow.countDocuments(filter);
+    const totalPages = Math.ceil(totalQuestions / limit);
+
+    const stackOverFlowQuestions = await stackOverFlow.find(filter)
+    .sort(sortFilter)
+    .limit(limit)
+    .skip(skip);
+
+
+    return res.json({
+        data: stackOverFlowQuestions,
+        total: totalQuestions,
+        page: page ? parseInt(page) : 1,
+        totalPages: totalPages
+
+    });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
 
 const updateStackOverFlowQuestion = async (req, res) => {
-    try {
-        res.send("Update Question");
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        
-    }
-}
+  try {
+    res.send("Update Question");
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
 
 const deleteStackOverFlowQuestion = async (req, res) => {
-    try {
-        res.send("Delete Question");
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        
-    }
-}
+  try {
+    res.send("Delete Question");
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
 
-export default { getStackOverFlowData, getStackOverFlowQuestions, updateStackOverFlowQuestion, deleteStackOverFlowQuestion };
+export default {
+  getStackOverFlowData,
+  getStackOverFlowQuestions,
+  updateStackOverFlowQuestion,
+  deleteStackOverFlowQuestion,
+};
